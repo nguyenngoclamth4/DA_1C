bd1e345cc06467f0e389798ce394d3fb2c238a19
nguyenngoclamth4 <54504636+nguyenngoclamth4@users.noreply.github.com>
2019-11-13 13:20:40 +0700

    lam

---
+++ DA_1/DA_1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace DA_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtđiaiem = new System.Windows.Forms.TextBox();
+            this.btnthem = new DevExpress.XtraEditors.SimpleButton();
+            this.btnxoa = new DevExpress.XtraEditors.SimpleButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lvDanhSachDiem = new System.Windows.Forms.ListView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.btntinh = new DevExpress.XtraEditors.SimpleButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(20, 19);
+            this.plotView1.Margin = new System.Windows.Forms.Padding(5);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(975, 688);
+            this.plotView1.TabIndex = 0;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1003, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Địa điểm đến:";
+            // 
+            // txtđiaiem
+            // 
+            this.txtđiaiem.Location = new System.Drawing.Point(1121, 30);
+            this.txtđiaiem.Name = "txtđiaiem";
+            this.txtđiaiem.Size = new System.Drawing.Size(237, 29);
+            this.txtđiaiem.TabIndex = 2;
+            // 
+            // btnthem
+            // 
+            this.btnthem.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnthem.ImageOptions.Image")));
+            this.btnthem.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.btnthem.Location = new System.Drawing.Point(1122, 74);
+            this.btnthem.Name = "btnthem";
+            this.btnthem.Size = new System.Drawing.Size(116, 30);
+            this.btnthem.TabIndex = 3;
+            this.btnthem.Text = "Thêm địa điểm";
+            this.btnthem.Click += new System.EventHandler(this.btnthem_Click);
+            // 
+            // btnxoa
+            // 
+            this.btnxoa.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnxoa.ImageOptions.Image")));
+            this.btnxoa.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.btnxoa.Location = new System.Drawing.Point(1244, 74);
+            this.btnxoa.Name = "btnxoa";
+            this.btnxoa.Size = new System.Drawing.Size(116, 30);
+            this.btnxoa.TabIndex = 4;
+            this.btnxoa.Text = "Xoá địa điểm";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lvDanhSachDiem);
+            this.groupBox1.Location = new System.Drawing.Point(1003, 146);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(355, 253);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ma trận trọng số:";
+            // 
+            // lvDanhSachDiem
+            // 
+            this.lvDanhSachDiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvDanhSachDiem.GridLines = true;
+            this.lvDanhSachDiem.HideSelection = false;
+            this.lvDanhSachDiem.Location = new System.Drawing.Point(6, 28);
+            this.lvDanhSachDiem.Name = "lvDanhSachDiem";
+            this.lvDanhSachDiem.Size = new System.Drawing.Size(343, 219);
+            this.lvDanhSachDiem.TabIndex = 7;
+            this.lvDanhSachDiem.UseCompatibleStateImageBehavior = false;
+            this.lvDanhSachDiem.View = System.Windows.Forms.View.Details;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rtbLog);
+            this.groupBox2.Location = new System.Drawing.Point(1003, 452);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(349, 231);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log";
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(6, 28);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(337, 197);
+            this.rtbLog.TabIndex = 1;
+            this.rtbLog.Text = "";
+            // 
+            // btntinh
+            // 
+            this.btntinh.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btntinh.ImageOptions.Image")));
+            this.btntinh.ImageOptions.ImageToTextAlignment = DevExpress.XtraEditors.ImageAlignToText.LeftCenter;
+            this.btntinh.Location = new System.Drawing.Point(1146, 405);
+            this.btntinh.Name = "btntinh";
+            this.btntinh.Size = new System.Drawing.Size(116, 30);
+            this.btntinh.TabIndex = 11;
+            this.btntinh.Text = "Tính đường đi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 727);
+            this.Controls.Add(this.btntinh);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnxoa);
+            this.Controls.Add(this.btnthem);
+            this.Controls.Add(this.txtđiaiem);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.plotView1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtđiaiem;
+        private DevExpress.XtraEditors.SimpleButton btnthem;
+        private DevExpress.XtraEditors.SimpleButton btnxoa;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView lvDanhSachDiem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox rtbLog;
+        private DevExpress.XtraEditors.SimpleButton btntinh;
+    }
+}
+
---
+++ DA_1/DA_1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+
+
+namespace DA_1
+{
+    public partial class Form1 : Form
+    {
+        PlotModel pm = new PlotModel();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //PlotView pv = new PlotView();
+            //plotView1.Dock = DockStyle.Fill;
+            this.Controls.Add(plotView1);
+
+            LinearAxis Xaxis = new LinearAxis { Position = OxyPlot.Axes.AxisPosition.Bottom, MajorGridlineStyle = LineStyle.Solid, MinorGridlineStyle = LineStyle.Dot, Minimum = 0, Maximum = 200 };
+            LinearAxis Yaxis = new LinearAxis { Position = AxisPosition.Left, MajorGridlineStyle = LineStyle.Solid, MinorGridlineStyle = LineStyle.Dot, Minimum = 0, Maximum = 200};
+            //PlotModel pm = new PlotModel();
+
+            pm.Axes.Add(Xaxis);
+            pm.Axes.Add(Yaxis);
+
+            plotView1.Model = pm;
+
+
+            this.plotView1.Model = pm; //plot1 is an object of PlotView
+        }
+
+        private void btnthem_Click(object sender, EventArgs e)
+        {
+            if (txtđiaiem.Text == "")
+            {
+                MessageBox.Show("Nhập địa điểm đến.");
+            }
+            else
+            {
+                int cot = 500;
+                int dong = 500;
+                //plotView1.Model.Series.Add(getFuntion());
+                int[,] scores = new int[cot, dong];
+                var series1 = new LineSeries
+                {
+                    StrokeThickness = 1,
+                    MarkerSize = 1,
+                };
+                Random _r = new Random();
+                int m = _r.Next(0, 50);
+                int n = _r.Next(0, 50);
+                int x_val = n;
+                int y_val = m;
+
+                //int x_val1 = n;
+                //int y_val1 = scores[0, n];
+                //"scores" is an array with my data
+                // PlotModel pm = new PlotModel();
+                series1.Points.Add(new DataPoint(x_val, y_val));
+                //series1.Points.Add(new DataPoint(x_val1, y_val1));
+                series1.Color = OxyColors.LightBlue;
+                pm.Series.Add(series1);
+                //var s1 = new LineSeries();
+
+                series1.Color = OxyColors.LightBlue;
+                series1.MarkerFill = OxyColors.Blue;
+                series1.MarkerType = OxyPlot.MarkerType.Circle;
+                series1.MarkerSize = 5;
+                txtđiaiem.Clear();
+                //plotView1.Model = pm;
+                //this.plotView1.Model = pm;
+
+            }
+        }
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            DataPoint point = (DataPoint)values[0];
+            List<DataPoint> points = ((IEnumerable<DataPoint>)values[1]).ToList();
+            return (points.IndexOf(point) + 1).ToString() + "-  (" + point.X.ToString() + " , " + point.Y.ToString() + ")";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
